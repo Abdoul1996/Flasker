1950e6b9b58e45dc3bb54eb392403bb0da8a76ad
Abdoul Abdillahi <aabdillahi@mail.sfsu.edu>
2024-03-26 16:52:54 -0700

    added navbar, name templates

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Djibouti"   # store in the app 
+
+
+# Create a Form Class 
+#csrf( it creates a screte key on the form)
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorator 
 @app.route('/')
@@ -37,6 +49,24 @@ def page_not_found(e):
 
 # Internal Server Error 
 @app.errorhandler(500)
-def page_not_found500(e):
+def page500(e):
     return render_template("500.html"), 500
-        
+
+
+
+# Create name page 
+@app.route('/name', methods=['GET', 'POST'])
+
+def name():
+    name = None
+    form = NamerForm()
+    # Validate form 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successful")
+    return render_template("name.html", 
+                           name = name,
+                           form = form)
+
+
